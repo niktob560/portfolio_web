fefc39f74eff98ebc35d114d57c51d8f6ce73870
nikto_b <niktob560@gmail.com>
2020-11-22 17:57:55 +0300

    Provided descriptions

--- lib/root.dart
+++ lib/root.dart
@@ -14,31 +14,31 @@ class RootWidgetState extends State<RootWidget> {
   final skills = {
     Skill('Android', {
       Project(
-        'CityFauna',
-        'descriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescriptiondescription',
+        'CityFauna Beta',
+        'Have a dog but have no time to walk with it? We can fix it!\nCustom design',
         [0, 1, 2].map((e) => 'assets/android/cf/$e.jpg').toSet(),
       ),
       Project(
           'SiteDog',
-          'description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description description ',
+          'Watch for a changes in site code\nDone in 1 month\nCustom design',
           [0, 1, 2, 3].map((e) => 'assets/android/sitedog/$e.jpg').toSet(),
           youtubeVideoId: '4kaeSD2Rwv0'),
       Project(
           'Newsbuzz',
-          'description description description description description description description description description description description ',
+          'Upwork order for a REST API client with endless list\nDone in 1 week\nDesign provided',
           [0, 1, 2, 3, 4].map((e) => 'assets/android/newsbuzz/$e.jpg').toSet(),
           playMarketUrl:
               'https://play.google.com/store/apps/details?id=com.indonesia.newsbuzz',
           youtubeVideoId: 'YkE17AlgRxk'),
-      Project('SampleLogin', 'description',
+      Project('SampleLogin', 'Login blank screen with transitions test\nDone in 1 day\nCustom design',
           [0, 1, 2, 3].map((e) => 'assets/android/samplelogin/$e.jpg').toSet(),
           youtubeVideoId: 'KdL6cgPIfm0'),
-      Project('STAVRIDE', 'description',
+      Project('STAVRIDE', 'AVR emulator for Android, the final project of Samsung IT School\nFull emulation of MCU\nDone in 1 month\nCustom design',
           [0, 1, 2, 3, 4].map((e) => 'assets/android/stavride/$e.jpg').toSet(),
           youtubeVideoId: 'm2IB5Tn9VvM'),
       Project(
           'HLUS',
-          'description',
+          'Interactive box office for a Hong Kong waffles shop\nProject paused, the customer went bankrupt\nCustom design, colors provided',
           [0, 1, 2, 3, 4, 5, 6, 7]
               .map((e) => 'assets/android/hlus/$e.jpg')
               .toSet()),
@@ -46,10 +46,10 @@ class RootWidgetState extends State<RootWidget> {
     Skill('Flutter', {
       Project(
         'QR scanner',
-        'description',
+        'The simplest QR scanner for Google Play Market\nDone in 1 day',
         [0, 1, 2].map((e) => 'assets/flutter/qr_scanner/$e.jpg').toSet(),
       ),
-      Project('Portfolio web site', 'description',
+      Project('Portfolio web site', 'This site\nDone in 1 week',
           [0, 1, 2].map((e) => 'assets/flutter/portf/$e.jpg').toSet(),
           githubUrl: 'https://github.com/niktob560/portfolio_web'),
     }),
