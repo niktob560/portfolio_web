438bce09e6ada59a4f422761c75737ef1c5eeb87
nikto_b <niktob560@gmail.com>
2020-11-22 17:30:32 +0300

    Added portfolio to projects, added disclaimer dialog

--- lib/root.dart
+++ lib/root.dart
@@ -36,8 +36,12 @@ class RootWidgetState extends State<RootWidget> {
       Project('STAVRIDE', 'description',
           [0, 1, 2, 3, 4].map((e) => 'assets/android/stavride/$e.jpg').toSet(),
           youtubeVideoId: 'm2IB5Tn9VvM'),
-      Project('HLUS', 'description',
-          [0, 1, 2, 3, 4, 5, 6, 7].map((e) => 'assets/android/hlus/$e.jpg').toSet()),
+      Project(
+          'HLUS',
+          'description',
+          [0, 1, 2, 3, 4, 5, 6, 7]
+              .map((e) => 'assets/android/hlus/$e.jpg')
+              .toSet()),
     }),
     Skill('Flutter', {
       Project(
@@ -45,6 +49,9 @@ class RootWidgetState extends State<RootWidget> {
         'description',
         [0, 1, 2].map((e) => 'assets/flutter/qr_scanner/$e.jpg').toSet(),
       ),
+      Project('Portfolio web site', 'description',
+          [0, 1, 2].map((e) => 'assets/flutter/portf/$e.jpg').toSet(),
+          githubUrl: 'https://github.com/niktob560/portfolio_web'),
     }),
     Skill('C++', {}),
     Skill('C', {}),
@@ -58,27 +65,64 @@ class RootWidgetState extends State<RootWidget> {
       _currentSkill != null ? _currentSkill : skills.first;
 
   @override
-  Widget build(BuildContext context) => ResponsiveScaffold(
-        title: Text('${currentSkill.name} projects'),
-        drawer: ListView(
-            children: [
-          DrawerHeader(
-            child: Text(
-              'Skills:',
-              style: TextStyle(color: Colors.white, fontSize: 32),
+  Widget build(BuildContext context) {
+    return ResponsiveScaffold(
+      title: Text('${currentSkill.name} projects'),
+      drawer: ListView(
+          children: [
+        DrawerHeader(
+          child: Text(
+            'Skills:',
+            style: TextStyle(color: Colors.white, fontSize: 32),
+          ),
+          decoration: BoxDecoration(color: Colors.blue),
+        )
+      ]..addAll(skills
+              .map((final e) => ListTile(
+                    title: Text(e.name),
+                    subtitle: Text('Projects: ${e.projects.length}'),
+                    onTap: () => setState(() => _currentSkill = e),
+                    selected: e == currentSkill,
+                  ))
+              .toList())),
+      body: Center(
+        child: SkillWidget(currentSkill),
+      ),
+    );
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(seconds: 1)).then((value) => _showDisclaimerDialog());
+  }
+
+  Future<void> _showDisclaimerDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Disclaimer'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('This site is under development.'),
+              ],
             ),
-            decoration: BoxDecoration(color: Colors.blue),
-          )
-        ]..addAll(skills
-                .map((final e) => ListTile(
-                      title: Text(e.name),
-                      subtitle: Text('Projects: ${e.projects.length}'),
-                      onTap: () => setState(() => _currentSkill = e),
-                      selected: e == currentSkill,
-                    ))
-                .toList())),
-        body: Center(
-          child: SkillWidget(currentSkill),
-        ),
-      );
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
